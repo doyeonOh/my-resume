0a0dd6007cc2339145be6550d9e0e6382e554e50
DoyeonOh <odyody12@gmail.com>
2016-06-16 17:06:26 +0900

    materialize-css package 추가 및 경력 추가 및 포트폴리오 샘플 구현

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 
 import { SkillsComponent } from './components/skills.component';
@@ -96,7 +96,7 @@ import { TranslateElement } from './directives/translate-element';
             <h5>컴퓨터 공학과 학사 졸업</h5>
           </h3>
           <p style="margin-top:2em;">
-            학과 내의 있는 SoftLab 에서 안드로이드 프로젝트를 주로 진행하였습니다. 그 중 4개의 App 을 퍼블리싱 하였습니다.
+            학과 내의 있는 SoftLab 에서 <b>안드로이드 프로젝트</b>를 주로 진행하였습니다. 그 중 4개의 App 을 퍼블리싱 하였습니다.
           </p>
           <p style="background: #fff">
             |
@@ -110,12 +110,73 @@ import { TranslateElement } from './directives/translate-element';
     </section>
     <section translate-element #third="tfe" [startY]="-987" (mousewheel)="moveSection($event, second, third)" id="third" style="z-index:7">
       <div>
-        2222222222222222
+        <h2 style="display:block; top:30%;left:40%;">
+          Work Experience
+        </h2>
+        <div style="position:absolute; left:40%; top:40%; width:20%;">
+          <h3 style="display:block; line-height:1.5; ">
+            <p style="display:inline;"><b>웹케시</b> <span style="font-size:0.5em;;text-align:right;">2013.1 ~ 2015.10 (2년 10개월)</span></p>
+            <br>
+            <h5>WEB DEVELOPER</h5>
+          </h3>
+          <p style="margin-top:2em;">
+            유치원 통합 업무 서비스인 <b>아이모아 서비스</b>와 재무회계 서비스인 <b>Sbook 프로젝트</b>를 진행하였습니다.
+          </p>
+          <p>
+            - Java, Jsp/Servlet, Javascript 이용한 웹 페이지 개발
+            <br>
+            - DDL, DML 사용 및 프로시져 구현 (PostgreSQL)
+            <br>
+            - User interaction 을 위한 모듈 구현(jQuery)
+            <br>
+            - 사내 여러 서비스 팀과 API 연계를 통한 협업
+          </p>
+          <p style="background: #fff">
+            |
+            <a target="_blank" href="https://www.imore.co.kr/">아이모아 서비스</a>,
+            <a target="_blank" href="https://www.bizplay.co.kr/main_0001_04.act">sbook 서비스(s재무회계)</a>
+          </p>
+        </div>
       </div>
     </section>
     <section translate-element #fourth="tfe" [startY]="-987" (mousewheel)="moveSection($event, third, fourth)" id="fourth" style="z-index:6">
       <div>
-        2222222222222222
+        <h2 style="display:block; top:23%;left:30%;">
+          포트폴리오
+        </h2>
+        <div style="position:absolute; top:30%; left:30%;float:left;">
+          <div style="position:relative;display:block; padding-right:1em;width:25em;float:left;">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="resources/img/portpolio/settle2.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Sbook 재무회계 서비스<i class="material-icons right">more_vert</i></span>
+                <p><a href="#">사이트로 이동</a></p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Sbook 재무회계 서비스<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              </div>
+            </div>
+          </div>
+
+          <div style="position:relative;display:block;padding-right:1em;width:25em;float:left;">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="resources/img/portpolio/imore2.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">아이모아 서비스<i class="material-icons right">more_vert</i></span>
+                <p><a href="#">사이트로 이동</a></p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">아이모아 서비스<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <section translate-element #fifth="tfe" [startY]="-987" (mousewheel)="moveSection($event, fourth, fifth)" id="fifth" style="z-index:5">
@@ -168,10 +229,10 @@ import { TranslateElement } from './directives/translate-element';
     section#main{     background-color: #2F292B;  color: white; }
     section#mid{      background-color: #01a5b1;  color:white;  }
     section#end{      background-color: #F2E0C8; }
-    section#third{    background-color: #FFF }
-    section#fourth{   background-color: #FFF }
-    section#fifth{    background-color: #FFF }
-    section#sixth{    background-color: #FFF }
+    section#third{    background-color: #E6E3FE; }
+    section#fourth{   background-color: #3A7AA6;  color:white; }
+    section#fifth{    background-color: #95836D }
+    section#sixth{    background-color: #EED9AC }
     .left{
       width:30%;
       left:30%;
@@ -204,7 +265,7 @@ import { TranslateElement } from './directives/translate-element';
   `]
 })
 
-export class AppComponent{
+export class AppComponent implements OnInit{
 
   moveSection(e, prev, next, num){
     let moveToTop = e.deltaY > 0;
@@ -214,6 +275,9 @@ export class AppComponent{
       prev.toggle(moveToTop);
     }
   }
+  ngOnInit(){
+    // document.getElementsByClassName('materialboxed').materialbox();
 
+  }
 
 }
