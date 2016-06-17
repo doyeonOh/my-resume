053f1a2e3a5ac607f1b580047a4dc076ee7d9317
DoyeonOh <odyody12@gmail.com>
2016-06-17 20:27:35 +0900

    image-zoom-element 리팩토링

--- app/directives/image-zoom-element.ts
+++ app/directives/image-zoom-element.ts
@@ -13,10 +13,13 @@ export class ImageZoomElement{
 
   private _isRunning: Boolean = false;
   private _isDisplayed: Boolean = false;
-  private _placeholder: HTMLElement;
 
   constructor(private _ab: AnimationBuilder, private _el: ElementRef) {
-    // placeholder 지정해야 이전 view 가 안깨짐
+    this.setPlaceholder();
+  }
+
+  setPlaceholder(){
+    // placeholder div tag 추가 - placeholder 지정해야 이전 view 가 안깨짐
     let el = this._el.nativeElement;
     var placeholder = document.createElement("div");
     placeholder.setAttribute("class", "image-zoom-placeholder");
@@ -24,32 +27,41 @@ export class ImageZoomElement{
     // wrapping
     el.parentNode.insertBefore(placeholder, el);
     placeholder.appendChild(el);
-
-    this._placeholder = placeholder;
   }
 
   clickElement(e){
     this.toggle();
   }
+
+  private changeOverflowAttribute(placeholder){
+    var ancestorsChanged = undefined;
+    var ancestor = placeholder.parentNode;
+    var count = 0;
+    while (ancestor !== null  && ancestor.tagName !== "BODY" ) {
+      var curr = ancestor;
+      if (curr.style.overflow !== 'visible') {
+        curr.style.overflow = 'visible';
+        if (ancestorsChanged === undefined) {
+          ancestorsChanged = curr;
+        }
+        else {
+          // ancestorsChanged = ancestorsChanged.add(curr);
+        }
+      }
+      ancestor = ancestor.parentNode;
+    }
+  }
   toggle(){
     if( !this._isRunning){
-      let animation = this._ab.css();
-      let animation2 = this._ab.css();
-      let el;
+      let animationImage = this._ab.css();
+      let animationOverlay = this._ab.css();
       let placeholder;
       let overlay;
 
-      // classname 이  placeholder  인것 가져오기 (순수 javascript 로 짜니 코드가 드러워진다..)
-      while ((el = this._el.nativeElement.parentElement) && !el.classList.contains("image-zoom-placeholder"));
-        placeholder = el;
+      // 생성자에 추가한 classname 이  placeholder  인것 가져오기
+      placeholder = this.getPlaceholderElement();
+      placeholder = this.stylingPlaceholder(placeholder); // styiling
 
-
-      // styiling
-      placeholder.style.width     = placeholder.getBoundingClientRect().width + "px";
-      placeholder.style.height    = placeholder.getBoundingClientRect().height + "px";
-      placeholder.style.position  = "relative";
-      placeholder.style.top       = "0";
-      placeholder.style.left      = "0";
 
       let inDuration = 275;
       let outDuration = 200;
@@ -81,125 +93,137 @@ export class ImageZoomElement{
       let newScaleY   = newHeight / originalHeight;
 
       if(this._isDisplayed){
-        animation.setDuration(outDuration);
-        animation.setFromStyles({
+        // 기image close 되는 animation
+        animationImage.setDuration(outDuration);
+        animationImage.setFromStyles({
           transform : "translate(" + newLeft + "px," + newTop  + "px" + ") scale(" + newScaleX + "," + newScaleY + ")",
           position  : "absolute",
           "z-index" : 1000,
           opacity   : 1 }
         );
-        animation.setToStyles({
+        animationImage.setToStyles({
           transform: "scale(1)",
           "z-index" : "",
           position: "relative",
           opacity: 1 });
+
+        // overlay close 되는 animation
+        animationOverlay.setDuration(outDuration);
+        animationOverlay.setFromStyles({
+          opacity: 1, top: 0, left: 0, right: 0, bottom: 0,
+          "background-color"  : "#292929",
+          "z-index"           : 1000,
+          position            : "fixed",
+          "will-change"       : "opacity"
+        });
+        animationOverlay.setToStyles({
+          opacity: 0
+        });
+
+        // open animation 때 생성한 overlay element 가져오기
+        overlay = this.findOverlayElement();
+
         this._isDisplayed = false;
 
-        animation2.setDuration(outDuration);
-        animation2.setFromStyles({
-          opacity: 1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          "background-color": "#292929",
-          "z-index": 1000,
-          position: "fixed",
-          "will-change": "opacity"
-        });
-        animation2.setToStyles({
-          opacity: 0
-        });
-
-        overlay
-        var childNodes = this._el.nativeElement.parentNode.children;
-
-        for(var i = 0; i < childNodes.length; i++){
-          var node = childNodes[i];
-          if(node.className == "image-zoom-overlay")
-            overlay = node;
-        }
-
       }else{
-        animation.setDuration(inDuration);
-        animation.setFromStyles({
+        //  image open 하는 animation
+        animationImage.setDuration(inDuration);
+        animationImage.setFromStyles({
           transform: "scale(1)",
           "z-index" : "",
-          position: "relative",
-          opacity: 1 });
-        animation.setToStyles({
+          position  : "relative",
+          opacity   : 1
+        });
+        animationImage.setToStyles({
           transform : "translate(" + newLeft + "px," + newTop  + "px" + ") scale(" + newScaleX + "," + newScaleY + ")",
           position  : "absolute",
           "z-index" : 1000,
           opacity   : 1 }
         );
-        this._isDisplayed = true;
 
-        overlay = document.createElement("div");
-        overlay.setAttribute("class", "image-zoom-overlay");
-
-        this._el.nativeElement.parentNode.insertBefore(overlay, this._el.nativeElement);
-
-        animation2.setDuration(inDuration);
-        animation2.setFromStyles({
+        // overlay open animation 세팅
+        animationOverlay.setDuration(inDuration);
+        animationOverlay.setFromStyles({
           opacity: 0
         });
-        animation2.setToStyles({
-          opacity: 1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
+        animationOverlay.setToStyles({
+          opacity: 1, top: 0, left: 0, right: 0, bottom: 0,
           "background-color": "#292929",
-          "z-index": 1000,
-          position: "fixed",
-          "will-change": "opacity"
+          "z-index"         : 1000,
+          position          : "fixed",
+          "will-change"     : "opacity"
         });
 
-      }
+        // overlay div 생성하기
+        overlay = this.createOverlayElement();
 
+        this._isDisplayed = true;
+      }
 
       // 상위 노드의 overflow:hidden 없애기
-      var ancestorsChanged = undefined;
-      var ancestor = placeholder.parentNode;
-      var count = 0;
-      while (ancestor !== null  && ancestor.tagName !== "BODY" ) {
-        var curr = ancestor;
-        if (curr.style.overflow !== 'visible') {
-          curr.style.overflow = 'visible';
-          if (ancestorsChanged === undefined) {
-            ancestorsChanged = curr;
-          }
-          else {
-            // ancestorsChanged = ancestorsChanged.add(curr);
-          }
-        }
-        ancestor = ancestor.parentNode;
-      }
+      this.changeOverflowAttribute(placeholder);
 
       this._isRunning = true;
 
-      animation.start(this._el.nativeElement).onComplete(() => {
+      animationImage.start(this._el.nativeElement).onComplete(() => {
           this._isRunning = false;
           // 확대 꺼진 후 overlay 삭제
           if(!this._isDisplayed){
-            var childNodes = this._el.nativeElement.parentNode.children;
+            this.removeOverlayElement();
 
-            for(var i = 0; i < childNodes.length; i++){
-              var node = childNodes[i];
-              if(node.className == "image-zoom-overlay")
-                node.remove();
-            }
           }
-
-
       }).eventClearFunctions;
 
       // 번쩍 거리는 현상있어서 overlay 주석처리
-      // animation2.start(overlay).onComplete(() => {
+      // animationOverlay.start(overlay).onComplete(() => {
       //   console.log("good");
       // });
     }
   }
 
+
+  private getPlaceholderElement(){
+    var el;
+    while ((el = this._el.nativeElement.parentElement) && !el.classList.contains("image-zoom-placeholder"));
+      return el;
+  }
+
+  private stylingPlaceholder(placeholder){
+    placeholder.style.width     = placeholder.getBoundingClientRect().width + "px";
+    placeholder.style.height    = placeholder.getBoundingClientRect().height + "px";
+    placeholder.style.position  = "relative";
+    placeholder.style.top       = "0";
+    placeholder.style.left      = "0";
+
+    return placeholder;
+  }
+
+  private findOverlayElement(){
+    var childNodes = this._el.nativeElement.parentNode.children;
+
+    for(var i = 0; i < childNodes.length; i++){
+      var node = childNodes[i];
+      if(node.className == "image-zoom-overlay")
+        return node;
+    }
+  }
+
+  private createOverlayElement(){
+    let overlay = document.createElement("div");
+    overlay.setAttribute("class", "image-zoom-overlay");
+
+    this._el.nativeElement.parentNode.insertBefore(overlay, this._el.nativeElement);
+
+    return overlay;
+  }
+
+  private removeOverlayElement(){
+    var childNodes = this._el.nativeElement.parentNode.children;
+
+    for(var i = 0; i < childNodes.length; i++){
+      var node = childNodes[i];
+      if(node.className == "image-zoom-overlay")
+        node.remove();
+    }
+  }
 }
