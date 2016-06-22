e24b6dbebffb9ddabf481ceab25abd7598286093
DoyeonOh <odyody12@gmail.com>
2016-06-22 14:35:45 +0900

    컴포넌트, 템플릿, css 모듈화

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,127 +12,14 @@ import { AnimationService } from './services/animation.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <nav style="z-index:15;">
-      <div>
-        <ul>
-          <li #nav class="active">1</li>
-          <li #nav>2</li>
-          <li #nav>3</li>
-          <li #nav>4</li>
-        </ul>
-      </div>
-    </nav>
-    <section translate-element #first="tfe" [startY]="-987" (mousewheel)="moveSection($event, null, first);" id="first" style="z-index:9;">
-      <me [id]="1" [currentFocusId]="currentFocusId"></me>
-    </section>
-    <section translate-element #second="tfe" [startY]="-987" (mousewheel)="moveSection($event, first, second)" id="second" style="z-index:8">
-      <skills [id]="2" [currentFocusId]="currentFocusId"></skills>
-    </section>
-    <section translate-element #third="tfe" [startY]="-987" (mousewheel)="moveSection($event, second, third)" id="third" style="z-index:7">
-      <works [id]="3" [currentFocusId]="currentFocusId"></works>
-    </section>
-    <section translate-element #fourth="tfe" [startY]="-987" (mousewheel)="moveSection($event, third, null)" id="fourth" style="z-index:6">
-      <portpolio [id]="4" [currentFocusId]="currentFocusId"></portpolio>
-    </section>
-  `,
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
   directives: [
       TranslateElement, MeComponent, SkillsComponent, WorksComponent, PortpolioComponent
   ],
   providers: [
       AnimationService
   ],
-  styles: [`
-
-    section{
-      width:100%;
-      height:100%;
-      position:fixed;
-      background-repeat: no repeat;
-      background-position: center;
-      background-size: cover;
-      overflow: hidden;
-    }
-    h2{
-      font-size: 40px;
-      line-height: 1.2em;
-      position: absolute;
-    }
-    section#first{     background-color: #2F292B;  color: white; }
-    section#second{    background-color: #01a5b1;  color:white;  }
-    section#third{     background-color: #3A7AA6;  color:white;}
-    section#fourth{    background-color: #13b0a5;  color:white; }
-    section#fifth{     background-color: #b4d6a4; }
-    section#sixth{     background-color: #67bc9a; }
-    .left{
-      width:30%;
-      left:30%;
-      posiiton: relative;
-      float: left;
-    }
-    .pull-right{
-      float: right!important;
-      margin-right: -15px;
-    }
-    ul#navi{
-      float:left;
-      margin: 0;
-    }
-    ul>li{
-      display: block;
-      position: relative;
-    }
-    ul#navi>li{
-      float: left;
-      display: block;
-      position: relative;
-    }
-
-    ul#navi>li>a{
-      position: relative;
-      display: block;
-      padding: 10px 15px;
-    }
-    nav{
-      top:50%;
-      right: 34px;
-      position:fixed;
-      z-index:200;
-      box-shadow:none;
-      background-color: transparent;
-      margin:  0;
-      padding: 0;
-      border: 0;
-      width:0;
-      vertical-align: baseline;
-    }
-    nav ul li.active{
-      background: #fff;
-    }
-    nav ul li{
-      border: 2px solid rgba(255,255,255,0.4);
-      margin-bottom: 10px;
-      width: 12px;
-      height: 12px;
-      display: block;
-      border-radius: 50%;
-      -webkit-border-radius: 50%;
-      -moz-border-radius: 50%;
-      -ms-border-radius: 50%;
-      -o-border-radius: 50%;
-      box-sizing: border-box;
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      -ms-box-sizing: border-box;
-      -o-box-sizing: border-box;
-      text-indent: -9999px;
-      white-space: nowrap;
-      overflow: hidden;
-      cursor: pointer;
-      list-style-position: inside;
-    }
-
-  `]
 })
 
 export class AppComponent implements OnInit{
--- app/components/me.component.ts
+++ app/components/me.component.ts
@@ -5,53 +5,8 @@ import { AnimationService } from '../services/animation.service';
 
 @Component({
   selector: 'me',
-  template: `
-    <div style="position: absolute;left: 0;top: 0;margin: auto;width: 33%;height: 27%;right: 0;bottom: 0;">
-      <div #item class="left_item my-box">
-
-      </div>
-      <div class="right_item" #item>
-        <h2 class="right-top">
-          <b>오 도 연 | Doyeon Oh</b>
-          <br>
-          web developer
-        </h2>
-        <div class="right-bottom">
-            <p><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/odyody12" target="_blank" style="color:white">@odyody12</a></p>
-            <p><i class="fa fa-github fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/doyeonOh" target="_blank" style="color:white">@doyeonOh</a></p>
-            <p><i class="fa fa-paper-plane fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;odyody12@gmail.com</p>
-        </div>
-      </div>
-    </div>
-  `,
-  styles: [`
-    .left_item{
-      position:relative;
-      float:left;
-      margin:1em;
-    }
-    .right_item{
-      position:relative;
-      float:left;
-      margin:1em;
-    }
-    .right-top{
-      margin-top: 0.1em;
-      margin-bottom:1em;
-    }
-    .right-bottom{
-      line-height:1.1;
-    }
-    .my-box{
-      width:15em;
-      height:15em;
-      border: 1px solid white;
-      background-image: url('resources/img/mark/me2.jpg');
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: 15em 15em;
-    }
-  `],
+  templateUrl: 'app/templates/me.component.html',
+  styleUrls: ['app/css/me.component.css'],
   directives: [ TranslateElement ]
 })
 
